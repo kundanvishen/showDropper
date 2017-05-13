7792a75f8a096c3bee01372d45609564aebcd4af
Kundan Singh <kundan.singh979@gmail.com>
2017-05-14 04:03:51 +0530

    Subscription service

---
+++ public/services/subscription.js
@@ -0,0 +1,13 @@
+angular.module('MyApp')
+    .factory('Subscriptions', ['$http',
+        function($http) {
+            return {
+                subscribe: function(show, user) {
+                    return $http.post('/api/subscribe', { showId: show._id });
+                },
+                unsubscribe: function(show, user) {
+                    return $http.post('/api/unsubscribe', { showId: show._id })
+                }
+            };
+        }
+    ]);
