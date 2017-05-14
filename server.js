8185cbb89482262a5e41db59dad16e8d25ef2938
Kundan Singh <kundan.singh979@gmail.com>
2017-05-14 10:03:07 +0530

    Subscription

--- server.js
+++ server.js
@@ -256,6 +256,30 @@ app.post('/api/shows', function(req, res, next) {
     });
 });
 
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+    Show.findById(req.body.showId, function(err, show) {
+        if (err) return next(err);
+        show.subscribers.push(req.user.id);
+        show.save(function(err) {
+            if (err) return next(err);
+            res.send(200);
+        });
+    });
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+    Show.findById(req.body.showId, function(err, show) {
+        if (err) return callback(err);
+        var index = show.subscribers.indexOf(req.user.id);
+        show.subscribers.splice(index, 1);
+        show.save(function(err) {
+            if (err) return next(err);
+            res.send(200);
+        });
+    });
+});
+
+
 // redirect route
 app.get('*', function(req, res) {
     res.redirect('/#' + req.originalUrl);
