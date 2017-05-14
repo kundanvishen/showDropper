bdbbbb00b94fc0a4f27231669c71e40ae6f88484
Kundan Singh <kundan.singh979@gmail.com>
2017-05-14 08:22:23 +0530

    Server Side Authentication with Passport

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ var bcrypt = require('bcryptjs');
 var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var _ = require('lodash');
 
 var showSchema = new mongoose.Schema({
@@ -72,6 +75,34 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, '-password', function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+    User.findOne({ email: email }, function(err, user) {
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) return done(err);
+            if (isMatch) return done(null, user);
+            return done(null, false);
+        });
+    });
+}));
+
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) next();
+    else res.send(401);
+}
+
 mongoose.connect('localhost');
 
 var app = express();
@@ -81,10 +112,39 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+app.use(function(req, res, next) {
+    if (req.user) {
+        res.cookie('user', JSON.stringify(req.user));
+    }
+    next();
+});
+
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+    res.cookie('user', JSON.stringify(req.user));
+    res.send(req.user);
+});
+
+app.post('/api/signup', function(req, res, next) {
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save(function(err) {
+        if (err) return next(err);
+        res.send(200);
+    });
+});
+
+app.get('/api/logout', function(req, res, next) {
+    req.logout();
+    res.send(200);
 });
 
 
@@ -204,4 +264,8 @@ app.get('*', function(req, res) {
 app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.send(500, { message: err.message });
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
