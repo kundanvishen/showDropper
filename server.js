99d3b7c1441fc62634c1efcfd69aa6068a3f0643
Kundan Singh <kundan.singh979@gmail.com>
2017-05-15 20:15:55 +0530

    Prepared agenda for sending email notifications

--- server.js
+++ server.js
@@ -5,7 +5,10 @@
  * 
  */
 
-var CONFIG = require('./config.json');
+var CONFIG = require('./config.local.json');
+var mongoConnectionString = CONFIG.MONGO_CONNECTION_STRING;
+
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -20,9 +23,9 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var _ = require('lodash');
-
-var mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
 var agenda = require('agenda')({ db: { address: mongoConnectionString } });
+var sugar = require('sugar');
+var nodemailer = require('nodemailer');
 
 var showSchema = new mongoose.Schema({
     _id: Number,
@@ -110,6 +113,48 @@ function ensureAuthenticated(req, res, next) {
 // mongoose.connect('localhost');
 mongoose.connect(mongoConnectionString);
 
+// define agenda to send email alert to users
+agenda.define('send email alert', function(job, done) {
+    Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
+        var emails = show.subscribers.map(function(user) {
+            return user.email;
+        });
+
+        var upcomingEpisode = show.episodes.filter(function(episode) {
+            return new Date(episode.firstAired) > new Date();
+        })[0];
+
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+            service: 'SendGrid',
+            auth: { user: 'hslogin', pass: 'hspassword00' }
+        });
+
+        var mailOptions = {
+            from: 'Fred Foo ✔ <foo@blurdybloop.com>',
+            to: emails.join(','),
+            subject: show.name + ' is starting soon!',
+            text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
+                'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
+        };
+
+        smtpTransport.sendMail(mailOptions, function(error, response) {
+            console.log('Message sent: ' + response.message);
+            smtpTransport.close();
+            done();
+        });
+    });
+});
+
+agenda.start();
+
+agenda.on('start', function(job) {
+    console.log("Job %s starting", job.attrs.name);
+});
+
+agenda.on('complete', function(job) {
+    console.log("Job %s finished", job.attrs.name);
+});
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -179,7 +224,7 @@ app.get('/api/shows/:id', function(req, res, next) {
 
 // add new TV show to the database
 app.post('/api/shows', function(req, res, next) {
-    var apiKey = process.env.TVDB_API_KEY;
+    var apiKey = CONFIG.TVDB_API_KEY;
     var parser = xml2js.Parser({
         explicitArray: false,
         normalizeThings: true
@@ -256,6 +301,8 @@ app.post('/api/shows', function(req, res, next) {
                 }
                 return next(err);
             }
+            var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2 });
+            agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
             res.send(200);
         });
     });
